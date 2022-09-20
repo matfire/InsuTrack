809c38db5cd49afbc056401fe5486d891f7e8ea5
Matteo Gassend <mgassend@gmail.com>
2022-09-20 16:58:35 +0200

    fix: appwrite login

--- components/Layout.js
+++ components/Layout.js
@@ -1,3 +1,4 @@
+import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { useSetRecoilState } from "recoil";
 import userAtom from "../atoms/user.atom";
@@ -5,10 +6,19 @@ import { account } from "../utils/client";
 
 export default function Layout({ children }) {
   const setUser = useSetRecoilState(userAtom);
+  const router = useRouter();
 
   useEffect(() => {
-    const tmp = account.get();
-    setUser(tmp);
+    const getUser = async () => {
+      try {
+        const tmp = await account.get();
+        setUser(tmp);
+      } catch {
+        router.push("/login");
+      }
+    };
+
+    getUser();
   }, []);
 
   return <>{children}</>;
